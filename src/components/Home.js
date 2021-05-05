df87db7b8ec885d91db9bfc1bf4a4346f810538d
Gabriel Cardoso <gabrielfjggyn@gmail.com>
2021-05-05 16:53:13 -0300

    Added async storeg and validation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {
   View,
   Text,
@@ -7,17 +7,68 @@ import {
   Platform,
   ScrollView,
   Image,
+  Modal,
 } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useNavigation} from '@react-navigation/native';
+// import {navigate} from '../Navigation';
 import FocusableHighlight from '../components/focusable/FocusableHighlight';
 import FocusableOpacity from '../components/focusable/FocusableOpacity';
+import useStateRef from '../hooks/useStateRef';
 import logo from '../assets/icon.png';
 import Style from '../styles/Style';
 
 const Home = () => {
   const inputTextRef = useRef(null);
+  const showModalButtonRef = useRef(null);
+  const hideModalButtonRef = useRef(null);
 
   const [urlPlayer, setUrlPlayer] = useState('');
   const [zoom, setZoom] = useState('100');
+
+  const [modalVisibleRef, isModalVisible, setModalVisible] = useStateRef(false);
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    if (Platform.OS === 'web') {
+      // Prevent react navigation to handle back button is modal is visible
+      return navigation.addListener('beforeRemove', (e) => {
+        if (isModalVisible()) {
+          e.preventDefault();
+        }
+      });
+    }
+  }, []);
+
+  async function init() {
+    try {
+      const getUrl = await AsyncStorage.getItem('@storage_url');
+      const getZoom = await AsyncStorage.getItem('@storage_zoom');
+
+      setUrlPlayer(getUrl);
+
+      return setZoom(getZoom ? getZoom : '100');
+    } catch (error) {}
+  }
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  async function handleSubmit() {
+    if (!urlPlayer || !zoom) {
+      console.log(zoom);
+      return setModalVisible(true);
+    }
+
+    try {
+      await AsyncStorage.setItem('@storage_url', urlPlayer);
+      await AsyncStorage.setItem('@storage_zoom', zoom);
+
+      // navigate('Home');
+    } catch (error) {}
+  }
 
   return (
     <View style={Style.styles.right}>
@@ -95,9 +146,58 @@ const Home = () => {
           <View style={styles.component}>
             <View style={styles.container}>
               <View style={[styles.container, styles.horizontal]}>
+                <Modal
+                  animationType="slide"
+                  transparent={true}
+                  visible={modalVisibleRef.current}
+                  onShow={() => {
+                    console.log('Modal.onShow()');
+                    // No way to focus the hide button on AndroidTv
+                  }}
+                  onDismiss={() => {
+                    console.log('Modal.onDismiss()');
+                    // Required for web
+                    if (Platform.OS === 'web') {
+                      // Force focus on previous button
+                      if (showModalButtonRef) {
+                        showModalButtonRef.current.focus();
+                      }
+                    }
+                  }}
+                  onRequestClose={() => {
+                    console.log('Modal.onRequestClose()');
+                    setModalVisible(false);
+                  }}
+                  style={styles.modal}>
+                  <View style={styles.modalCenteredView}>
+                    <View style={styles.modalView}>
+                      <Text style={styles.modalText}>
+                        Atenção! É necessário preencher todos os campos.
+                      </Text>
+                      <FocusableHighlight
+                        ref={hideModalButtonRef}
+                        nativeID={'component_hide_modal_button'}
+                        focusable={modalVisibleRef.current}
+                        hasTVPreferredFocus={modalVisibleRef.current}
+                        onPress={() => {
+                          setModalVisible(false);
+                        }}
+                        style={styles.modalOpenButton}
+                        underlayColor={Style.buttonUnfocusedColor}
+                        styleFocused={{
+                          backgroundColor: Style.buttonFocusedColor,
+                        }}
+                        stylePressed={{
+                          backgroundColor: Style.buttonPressedColor,
+                        }}>
+                        <Text style={styles.buttonText}>Fechar</Text>
+                      </FocusableHighlight>
+                    </View>
+                  </View>
+                </Modal>
                 <FocusableOpacity
                   nativeID={'component_touchable_opacity'}
-                  onPress={() => {}}
+                  onPress={handleSubmit}
                   style={styles.button}
                   color={Style.buttonUnfocusedColor}
                   colorFocused={Style.buttonFocusedColor}
@@ -189,5 +289,41 @@ const styles = StyleSheet.create({
     fontSize: Style.px(58),
     fontWeight: 'bold',
     color: '#fff',
+  },
+  modal: {},
+  modalCenteredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    width: Style.px(500),
+    height: Style.px(300),
+    margin: 20,
+    backgroundColor: Style.modalBackgroundColor,
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  modalOpenButton: {
+    backgroundColor: Style.buttonUnfocusedColor,
+    borderRadius: Style.px(20),
+    padding: Style.px(10),
+    elevation: Style.px(2),
+  },
+  modalText: {
+    height: Style.px(130),
+    fontSize: Style.px(30),
+    color: 'white',
+    textAlign: 'center',
   },
 });
